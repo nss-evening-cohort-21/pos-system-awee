c26aa2ee1f5293125e8b456c4be24fb43462d00a
Allison Blumenthal <blumen.allison@gmail.com>
2022-12-10 14:24:17 -0500

    setting up login page

--- components/loginButton.js
+++ components/loginButton.js
@@ -3,7 +3,7 @@ import { signIn } from '../utils/auth';
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
   const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
-  document.querySelector('#login-form-container').innerHTML = domString;
+  document.querySelector('#login-button-container').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
---
+++ pages/loginPage.js
@@ -0,0 +1,19 @@
+import loginButton from '../components/loginButton';
+import logo from
+
+const loginPage = () => {
+  const domString = `
+  <div id="login-page">
+    <div id="logo-container">
+    <img src=${logo} alt="Hip Hop Pizza and Wangs Logo" width="200" height="80" id="hhpw-logo"/></div>
+    <div id="login-button-container"></div>
+
+  </div>
+  
+  
+  
+  
+  `
+};
+
+export default loginPage;

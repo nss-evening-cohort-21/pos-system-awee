5d3bb123874deac67a3ed38f85e4ea78bedca8ee
Eric Frey <ericlfrey@gmail.com>
2022-12-12 20:21:43 -0600

    Saving before merge

--- events/domEvents.js
+++ events/domEvents.js
@@ -5,7 +5,6 @@ const domEvents = () => {
   document.querySelector('#main').addEventListener('click', (e) => {
     // VIEW ORDERS PAGE
     if (e.target.id === 'viewOrdersBtn') {
-      console.warn('VIEW ORDERS');
       getAllOrders().then((orders) => {
         viewOrdersPage(orders);
       });
--- pages/viewOrdersPage.js
+++ pages/viewOrdersPage.js
@@ -20,8 +20,10 @@ const viewOrdersPage = (arr) => {
   <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title">${item.name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <h6 class="card-subtitle mb-2 text-muted">OrderStatus</h6>
+      <p class="card-text">Phone</p>
+      <p class="card-text">Email</p>
+      <p class="card-text">Type</p>
       <a href="#" class="card-link">Card link</a>
       <a href="#" class="card-link">Another link</a>
     </div>

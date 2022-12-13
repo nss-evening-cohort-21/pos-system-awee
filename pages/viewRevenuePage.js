0f56c642075c8140e7b859a6a23204b0a33ff43d
Whitley Beers <whitleybeers@gmail.com>
2022-12-12 19:18:22 -0600

    starting revenue page

---
+++ pages/viewRevenuePage.js
@@ -0,0 +1,14 @@
+import { getAllRevenue } from '../api/revenueData';
+import clearDOM from '../utils/clearDom';
+import renderToDOM from '../utils/renderToDOM';
+
+const revenuePage = (array) => {
+  clearDOM();
+  let domString = '';
+  array.forEach((item) => {
+
+  });
+  renderToDOM();
+};
+
+export default revenuePage();

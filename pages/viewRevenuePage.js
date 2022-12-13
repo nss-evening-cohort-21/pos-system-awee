67c72940a566b8e82ae54ab408f4a9418093e239
Whitley Beers <whitleybeers@gmail.com>
2022-12-12 20:00:19 -0600

    working on revenue page

--- pages/viewRevenuePage.js
+++ pages/viewRevenuePage.js
@@ -1,13 +1,3 @@
-import clearDOM from '../utils/clearDom';
-import renderToDOM from '../utils/renderToDOM';
-
-const revenuePage = (array) => {
-  clearDOM();
-  let domString = '<h1>REVENUE</h1><hr>';
-  array.forEach((item) => {
-    domString += `<h2>TOTAL REVENUE: ${item.total}</h2>`;
-  });
-  renderToDOM('#main', domString);
+const viewRevenuePage = (array) => {
+  
 };
-
-export default revenuePage();

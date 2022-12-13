4b4b208a045a7dce6038bd83b901056ff872040e
Eric Frey <ericlfrey@gmail.com>
2022-12-12 19:40:58 -0600

    Added the event listeners to the event listener files

--- events/navEvents.js
+++ events/navEvents.js
@@ -1,11 +1,16 @@
 const navEvents = () => {
-  // VIEW ORDERS
+  document.querySelector('#navBar').addEventListener('click', (e) => {
+    // VIEW ORDERS
+    if (e.target.id === 'view-orders') {
+      console.warn('VIEW ORDERS');
+    }
+    // VIEW CREATE ORDER PAGE
 
-  // VIEW CREATE ORDER PAGE
+    // VIEW REVENUE PAGE
 
-  // VIEW REVENUE PAGE
-
-  // LOGO/VIEW WELCOME PAGE
+    // LOGO/VIEW WELCOME PAGE
+  });
+  // SEARCH
 };
 
 export default navEvents;

b7a84f80cdb05523ec5dcb2c0a16017219c7ce97
Whitley Beers <whitleybeers@gmail.com>
2022-12-12 21:15:47 -0600

    merged Eric's page

--- pages/viewRevenuePage.js
+++ pages/viewRevenuePage.js
@@ -1,9 +1,8 @@
 import clearDOM from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDOM';
 
-const viewRevenuePage = (array) => {
+const viewRevenuePage = () => {
   clearDOM();
-  console.warn(array);
   const domString = '<h1>REVENUE</h1><hr><h3>TOTAL REVENUE: $</h3><ul style="list-style-type:none;"><li>Total Tips:</li><li>Total call in orders:</li><li>Total walk in orders:</li><br><li>Payment Types:</li><li>Cash - </li><li>Credit - </li><li>Mobile - </li></ul>';
 
   renderToDOM('#main', domString);

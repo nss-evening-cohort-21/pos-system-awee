11da3c4a5692d19a79a84c9c8997681c9dbdbd78
Erin Stephens <erebstephens@gmail.com>
2022-12-13 19:54:27 -0600

    fixed issues with the select order type drop down

--- pages/createOrderPage.js
+++ pages/createOrderPage.js
@@ -8,20 +8,20 @@ const createOrderForm = (obj = {}) => {
     <form id="${obj.firebaseKey ? `update-order--${obj.firebaseKey}` : 'submit-order'}">
       <div class="mb-3" style="text-align:left">
         <label for="orderName" class="form-label">Order Name</label>
-        <input type="text" class="form-control" id="orderName" value="${obj.name || ''}">
+        <input type="text" class="form-control" id="orderName" value="${obj.name || ''}" required>
       </div>
       <div class="mb-3" style="text-align:left">
         <label for="customerPhone" class="form-label">Customer Phone</label>
-        <input type="tel" class="form-control" id="phone" value="${obj.phone || ''}">
+        <input type="tel" class="form-control" id="phone" value="${obj.phone || ''}" required>
       </div>
       <div class="mb-3" style="text-align:left">
         <label for="email" class="form-label">Customer Email</label>
-        <input type="email" class="form-control" id="email" value="${obj.email || ''}">
+        <input type="email" class="form-control" id="email" value="${obj.email || ''}" required>
       </div>
       <div class="mb-3" style="text-align:left">
         <label for="orderType" class="form-label">Order Type</label>  
-        <select class="form-select" aria-label="Default select example" id="orderType">
-          <option selected>Select an Order Type</option>
+        <select class="form-select" aria-label="Default select example" id="orderType" required>
+          <option selected disabled value="">Select an Order Type</option>
           <option value="in-person"${obj.orderType === 'in-person' ? 'selected' : ''}>In-Person</option>
           <option value="phone"${obj.orderType === 'phone' ? 'selected' : ''}>Phone</option>
         </select>

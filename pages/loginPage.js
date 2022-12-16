43927b5c15d63c22f4aedbdd1222a7bd814707b9
Allison Blumenthal <blumen.allison@gmail.com>
2022-12-15 21:07:25 -0500

    working on login page look

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -5,12 +5,13 @@ import background from '../assets/abstract_background.jpg';
 function loginPage() {
   const domString = `
   <div id="login-page">
-    <img src=${background} alt="Abstract, colorful background" width="100%" height="100%" id="login-page-background">
-    <img src=${logo} alt="Hip Hop Pizza and Wangs Logo" width="300" height="300" id="hhpw-logo"/>
-    <div id="login-button-container">
+    <div id="image-container" class="image-container">
+      <img src=${background} alt="Abstract, colorful background" width="100%" height="100%" id="login-page-background" class="background"/>
+      <img src=${logo} alt="Hip Hop Pizza and Wangs Logo" width="300" height="300" id="hhpw-logo" class="logo"/>
+      <div id="login-button-container" class="login-button"></div>
+    </div>
   </div>
-
-  </div>`;
+  `;
 
   document.querySelector('#app').innerHTML = domString;
   loginButton();

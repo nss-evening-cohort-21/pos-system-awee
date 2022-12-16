60c8725b3fb6f4aa67f9d177adfd27c8081c8a0e
Allison Blumenthal <blumen.allison@gmail.com>
2022-12-15 22:12:39 -0500

    working on buttons

--- components/shared/navBar.js
+++ components/shared/navBar.js
@@ -3,7 +3,7 @@ import logo from '../../assets/hhpw_logo.png';
 
 const navBar = () => {
   const domString = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning mb-5">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
     <div class="container-fluid">
         <a href="#">
           <img src=${logo} alt="Hip Hop Pizza and Wangs Logo" width="40px" height="40px" id="navbar-hhpw-logo"/>
@@ -27,7 +27,7 @@ const navBar = () => {
           </ul>
           <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="search">
-          <button class="btn btn-success" type="submit" id="searchBtn">Search</button>
+          <button class="btn btn-green" type="submit" id="searchBtn">Search</button>
         </form>
           <div id="logout-button"></div>
         </div>

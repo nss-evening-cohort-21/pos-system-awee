1acf986d368d0c514c2258d00e4dd22ce1459d38
Allison Blumenthal <blumen.allison@gmail.com>
2022-12-15 20:10:29 -0500

    got background to load, need to work on overlay

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -1,19 +1,19 @@
 import loginButton from '../components/loginButton';
 import logo from '../assets/hhpw_logo.png';
+import background from '../assets/abstract_background.jpg';
 
-const loginPage = () => {
+function loginPage() {
   const domString = `
   <div id="login-page">
-    <div id="logo-container">
+    <img src=${background} alt="Abstract, colorful background" width="100%" height="100%" id="login-page-background">
       <img src=${logo} alt="Hip Hop Pizza and Wangs Logo" width="300" height="300" id="hhpw-logo"/>
-    </div>
     <div id="login-button-container">
-    </div>
+  </div>
 
   </div>`;
 
   document.querySelector('#app').innerHTML = domString;
   loginButton();
-};
+}
 
 export default loginPage;
